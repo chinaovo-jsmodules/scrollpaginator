3d2cfe3e475b6613f8f142202d95062ed8f9f452
OvO海风OvO <linsf1979@163.com>
2016-04-27 13:31:59 +0800

    fix

--- src/scrollpaginator.js
+++ src/scrollpaginator.js
@@ -33,59 +33,80 @@
             if (options && typeof (options.currentPage) !== 'undefined') {
                 this.setCurrentPage(options.currentPage);
             }
-
-            if(this.height>this.$element.height()){
-                this.$element.css({'min-height': (this.height+10) + 'px'});
+            this.$scrollWin.css({'height': this.height + 'px', 'overflow-y': 'auto','display': 'block','padding':'0px'});
+            if (this.height > this.$element.height()) {
+                this.$element.css({'min-height': (this.height + 25) + 'px', 'display': 'block','overflow': 'hidden','margin':'0px'});
             }
 
-            this.$scrollWin.css({'height': this.height + 'px', 'overflow-y': 'auto'}).scrollTop(0);
+            this.$scrollWin.scrollTop(0);
 
             this.bindEvent();
         },
         setCurrentPage: function (page) {
-            this.currentPage = parseInt(page, 10);
+            var cpage = parseInt(page, 10);
+            if (cpage == 1) {
+                this.$element.empty();
+                this.$scrollWin.scrollTop(0);
+                $('.loadmsg', this.$scrollWin).remove();
+            }
+            this.currentPage = cpage
+
         },
         onScrollBottomed: function (evt, page) {
-            var loadmsg = $('.loadmsg', this.$scrollWin);
-            if (loadmsg.length < 1) {
+            if (!this.haveMsg()) {
                 if (typeof (this.options.onScrollBottomed) === "function") {
                     this.options.onScrollBottomed(++this.currentPage)
                 }
             } else {
-                loadmsg.show();
-                setTimeout(function () {
-                    loadmsg.hide()
-                }, 5000);
+                this.showMsg();
             }
         },
         haveData: function (bool) {
             if (bool == false) {
                 this.currentPage--;
-                var loadmsg = $('.loadmsg', this.$scrollWin);
-                if (loadmsg.length < 1) {
-                    loadmsg = $("<div class='loadmsg'>").css({
-                        'text-align': 'center',
-                        'padding': '5px;'
-                    }).text('已经到底了');
-                    loadmsg.appendTo(this.$scrollWin);
-                } else {
-                    loadmsg.show();
-                }
-                setTimeout(function () {
-                    loadmsg.hide()
-                }, 5000);
+                this.showMsg();
             }
+
+            if (bool == true) {
+                $('.loadmsg', this.$scrollWin).remove();
+            }
+        },
+        haveMsg:function(){
+            var loadmsg = $('.loadmsg', this.$scrollWin);
+            if (loadmsg.length < 1) {
+                return false;
+            }
+            return true;
+        },
+        showMsg:function(){
+            var _$scrollWin=this.$scrollWin;
+            var _scrolltop=_$scrollWin.scrollTop();
+            var loadmsg = $('.loadmsg', this.$scrollWin);
+            if (loadmsg.length < 1) {
+                loadmsg = $("<div class='loadmsg'>").css({'text-align': 'center','padding': '5px;',"clear":'both'}).text('已经到底了');
+                loadmsg.appendTo(this.$scrollWin);
+            } else {
+                loadmsg.show();
+            }
+            //console.debug('_scrolltop='+_scrolltop);
+            setTimeout(function () {
+                loadmsg.hide();
+                //console.debug('$scrollWin1='+_$scrollWin.scrollTop());
+                _$scrollWin.scrollTop(_$scrollWin.scrollTop()-1);
+                //console.debug('$scrollWin2='+_$scrollWin.scrollTop());
+            }, 3000);
         },
         bindEvent: function () {
             var _this = this;
             var _scrollelement = this.$element;
             this.$scrollWin.unbind('scroll');
             this.$scrollWin.bind('scroll', function () {
-
                 var scrollTop = $(this).scrollTop();
                 var scrollHeight = _scrollelement.height();
                 var winHeight = $(this).height();
-                if (scrollTop + winHeight > scrollHeight) {//滚动到底部执行事件
+                //console.debug('w.s=' + scrollTop + ',w.h=' + winHeight + ',s+h=' + (scrollTop + winHeight) + ',s.h=' + scrollHeight + '');
+                if (scrollTop + winHeight ==scrollHeight) {//滚动到底部执行事件
+                    //console.debug('onScrollBottomed');
                     _this.onScrollBottomed();
                 }
                 if (scrollTop == 0) {//滚动到头部部执行事件
@@ -108,16 +129,7 @@
                 $this.data('scrollPaginator', data);
                 return;
             }
-            if (typeof option === 'string') {
-                if (data[option]) {
-                    result = data[option].apply(data, Array.prototype.slice.call(args, 1));
-                } else {
-                    throw "Method " + option + " does not exist";
-                }
-
-            } else {
-                result = data.setOptions(option);
-            }
+            result = data.setOptions(option);
         });
 
         return result;
